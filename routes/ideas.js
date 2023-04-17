239862ad94309ade123d870c24239c9823ba5096
Alex Gerhardt <a.gerhardt04@gmail.com>
2023-04-16 19:58:24 -0500

    backend API working with mongoDB database

---
+++ routes/ideas.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const Idea = require('../models/Idea');
+
+//get all ideas
+router.get('', async (req, res) => {
+  try {
+    const ideas = await Idea.find();
+    res.json({ success: true, data: ideas });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'something went wrong' });
+    console.log(error);
+  }
+});
+
+//get single idea
+router.get('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    res.json({ success: true, data: idea });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'something went wrong' });
+    console.log(error);
+  }
+});
+
+// add an idea
+router.post('/', async (req, res) => {
+  const idea = new Idea({
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+  });
+  try {
+    const savedIdea = await idea.save();
+    res.json({ success: true, data: savedIdea });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'something went wrong' });
+    console.log(error);
+  }
+});
+
+//update idea
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          text: req.body.text,
+          tag: req.body.tag,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'something went wrong' });
+  }
+});
+
+//delete an idea
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.json({ success: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'something went wrong' });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 const app = express();
+
+//body parser middlewear
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
+
+const ideaRouter = require('./routes/ideas');
+app.use('/api/ideas', ideaRouter);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
